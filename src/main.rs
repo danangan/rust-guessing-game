db352c2cdf671deb1152f96c16062a7fbf17f261
Danang Nur <danang.arbansa@personio.de>
2020-06-02 23:47:32 +0200

    finalizing

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,27 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1,101);
 
     println!("Secret number {}", secret_number);
 
 
     loop {
+    println!("Please input your guess.");
+
         let mut guess = String::new();
 
         io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input!");
+                continue;
+            },
+        };
 
         println!("You guessed: {}", guess);
 
